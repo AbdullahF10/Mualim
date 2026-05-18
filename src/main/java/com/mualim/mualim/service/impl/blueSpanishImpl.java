9bb7f2a7d19f3c90d0f8da6b11a86aa125459869
Abdullah Fadliyah <fadliyahabdullah@sensetime.com.sa>
2026-05-18 16:23:55 +0300

    @Component annotation

--- src/main/java/com/mualim/mualim/service/impl/blueSpanishImpl.java
+++ src/main/java/com/mualim/mualim/service/impl/blueSpanishImpl.java
@@ -1,9 +1,9 @@
 package com.mualim.mualim.service.impl;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import com.mualim.mualim.service.Blue;
 
-@Service 
 public class blueSpanishImpl implements Blue {
 
      @Override
--- src/main/java/com/mualim/mualim/service/impl/greenSpanishImpl.java
+++ src/main/java/com/mualim/mualim/service/impl/greenSpanishImpl.java
@@ -3,7 +3,6 @@ package com.mualim.mualim.service.impl;
 import org.springframework.stereotype.Service;
 import com.mualim.mualim.service.Green;
 
-@Service 
 public class greenSpanishImpl implements Green {
 
     @Override
